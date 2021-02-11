b6b2fb49090aa3673327bb88af156626d12dd590
Adrián Martinez <adrimrtnz@gmail.com>
2021-02-11 09:47:28 +0100

    Enemy Collisions and Colliders from script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.AddComponent<BoxCollider>();
+    }
+    void OnParticleCollision(GameObject other)
+    {
+        //print("Bullet particles collided with enemy " + gameObject.name);
+        Destroy(gameObject);
+    }
+}
